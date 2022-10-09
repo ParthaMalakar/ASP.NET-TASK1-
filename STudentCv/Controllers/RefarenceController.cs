2b5ce6d63720153d4cc55b46f4b417f0fae78110
ParthaMalakar <parthamalakar4321@gmail.com>
2022-10-09 17:24:23 +0600

    first commit

---
+++ STudentCv/Controllers/RefarenceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STudentCv.Controllers
+{
+    public class RefarenceController : Controller
+    {
+        // GET: Refarence
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
